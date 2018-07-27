218ef42fb9a324b15e67408899593621dc738471
SaberNaut <darkninjanaut@gmail.com>
2018-07-27 14:39:12 -0400

    Updated assembly version

--- FFTrainer/ViewModels/MainViewModel.cs
+++ FFTrainer/ViewModels/MainViewModel.cs
@@ -195,7 +195,7 @@ namespace FFTrainer.ViewModels
             {
                 // sleep for 500 ms
                 Thread.Sleep(Properties.Settings.Default.Read);
-                // check if our memory manager is set
+                // check if our memory manager is set /saving
                if(!MainWindow.CurrentlySaving) mediator.SendWork();
 
             }
